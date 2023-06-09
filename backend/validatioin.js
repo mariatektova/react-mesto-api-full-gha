c2374be0f8ab6a8ed1d61edaff8addcd45192bcc
maria tektova <tkmarie@yandex.ru>
2023-06-09 16:22:19 +0300

    Update validatioin.js

--- backend/validatioin.js
+++ backend/validatioin.js
@@ -1,4 +1,4 @@
-const { celebrate, Joi } = require('celebrate');
+const { Joi } = require('celebrate');
 const { urlRegEx } = require('./utils/constants');
 
 const signinValidation = {
@@ -16,72 +16,89 @@ const signinValidation = {
 
 const signupValidation = {
   body: Joi.object().keys({
-    email: Joi.string().required().email().required()
-      .messages({
-        'string.email': 'Введена некорректная почта',
-        'any.required': 'Почта не должна быть пустой',
-      }),
+    email: Joi.string().required().email().messages({
+      'string.email': 'Введена некорректная почта',
+      'any.required': 'Почта не должна быть пустой',
+    }),
     password: Joi.string().required().min(8).messages({
       'string.password': 'Пароль должен быть не менее 8 символов',
       'any.required': 'Пароль не должен быть пустым',
     }),
-    name: Joi.string().required().min(2).max(30)
-      .messages({
-        'string.min': 'Поле "имя" не должно быть меньше 2 символов',
-        'string.max': 'Поле "имя" не должно быть больше 30 символов',
-        'any.required': 'Поле "имя" не должно быть пустым',
-      }),
-    about: Joi.string().required().min(2).max(30)
-      .messages({
-        'string.min': 'Поле "род деятельности" не должно быть меньше 2 символов',
-        'string.max': 'Поле "род деятельности" не должно быть больше 30 символов',
-        'any.required': 'Поле "род деятельности" не должно быть пустым',
-      }),
-    avatar: Joi.string().required().regex(urlRegEx).message('Невалидная ссылка'),
+    name: Joi.string().min(2).max(30).messages({
+      'string.min': 'Поле "имя" не должно быть меньше 2 символов',
+      'string.max': 'Поле "имя" не должно быть больше 30 символов',
+      'any.required': 'Поле "имя" не должно быть пустым',
+    }),
+    about: Joi.string().min(2).max(30).messages({
+      'string.min': 'Поле "род деятельности" не должно быть меньше 2 символов',
+      'string.max': 'Поле "род деятельности" не должно быть больше 30 символов',
+      'any.required': 'Поле "род деятельности" не должно быть пустым',
+    }),
+    avatar: Joi.string().regex(urlRegEx).message('Невалидная ссылка'),
   }),
 };
 
-const getUserByIdValidation = celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().length(24).hex().required(),
+const getUserByIdValidation = {
+  params: Joi.object({
+    userId: Joi.string().hex().length(24).message('Некорректный id'),
   }),
-});
+};
 
-const editProfileValidation = celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+const editProfileValidation = {
+  body: Joi.object({
+    name: Joi.string().min(2).max(30).messages({
+      'string.min': 'Поле "имя" не должно быть меньше 2 символов',
+      'string.max': 'Поле "имя" не должно быть больше 30 символов',
+      'any.required': 'Поле "имя" не должно быть пустым',
+    }),
+    about: Joi.string().min(2).max(30).messages({
+      'string.min': 'Поле "род деятельности" не должно быть меньше 2 символов',
+      'string.max': 'Поле "род деятельности" не должно быть больше 30 символов',
+      'any.required': 'Поле "род деятельности" не должно быть пустым',
+    }),
   }),
-});
+};
 
-const updateAvatarValidation = celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().pattern(urlRegEx).required(),
+const updateAvatarValidation = {
+  body: Joi.object({
+    avatar: Joi.string().regex(urlRegEx).message('Невалидная ссылка'),
   }),
-});
+};
 
-const createCardValidation = celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(urlRegEx),
+const createCardValidation = {
+  body: Joi.object({
+    name: Joi.string()
+      .min(2)
+      .max(30)
+      .messages({
+        'string.min': 'Название карточки не должно быть меньше 2 символов',
+        'string.max': 'Название карточки не должно быть больше 30 символов',
+        'any.required': 'Название карточки не должно быть пустым',
+      })
+      .required(),
+    link: Joi.string()
+      .regex(urlRegEx)
+      .messages({
+        'string.dataUri': 'Невалидная ссылка',
+        'any.required': 'Название карточки не должно быть пустым',
+      })
+      .required(),
   }),
-});
+};
 
 const deleteCardValidation = {
   params: Joi.object({
-    cardId: Joi.string().hex().length(24).required()
-      .messages({
-        'string.hex': 'Некорректный id',
-      }),
+    cardId: Joi.string().hex().length(24).messages({
+      'string.hex': 'Некорректный id',
+    }),
   }),
 };
 
 const likeCardValidation = {
   params: Joi.object({
-    cardId: Joi.string().hex().length(24).required()
-      .messages({
-        'string.hex': 'Некорректный id',
-      }),
+    cardId: Joi.string().hex().length(24).messages({
+      'string.hex': 'Некорректный id',
+    }),
   }),
 };
 
